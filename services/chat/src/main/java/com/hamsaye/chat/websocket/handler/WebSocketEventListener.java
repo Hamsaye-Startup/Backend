2add7d5074d57b7658ecd22ab37796642ce120df
pgt7 <s.beigi.79@gmail.com>
2024-08-19 20:18:58 +0330

    add websocket handler for tracking connectivity of user

---
+++ services/chat/src/main/java/com/hamsaye/chat/websocket/handler/WebSocketEventListener.java
@@ -0,0 +1,66 @@
+package com.hamsaye.chat.websocket.handler;
+
+import com.hamsaye.chat.users.models.ConnectionStatus;
+import com.hamsaye.chat.users.models.UserConnectionState;
+import com.hamsaye.chat.users.models.UserEntity;
+import com.hamsaye.chat.users.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    private final UserService userService;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+
+        // find the user from header
+        Object userId = headerAccessor.getHeader("X_USER_ID");
+
+        if (userId != null) {
+
+            // find the user
+            UserEntity user = userService.findUserById(UUID.fromString((String) userId));
+            userService.connectUser(user, UserConnectionState.builder()
+                    .socketId(sessionId)
+                    .initiatedAt(LocalDateTime.now())
+                    .connectionStatus(ConnectionStatus.CONNECTED)
+                    .build());
+
+            System.out.println("session is connected!!!");
+        }
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+
+        // find the user from header
+        Object userId = headerAccessor.getHeader("X_USER_ID");
+
+        if (userId != null) {
+
+            // find the user
+            UserEntity user = userService.findUserById(UUID.fromString((String) userId));
+            userService.connectUser(user, UserConnectionState.builder()
+                    .socketId(sessionId)
+                    .initiatedAt(LocalDateTime.now())
+                    .connectionStatus(ConnectionStatus.DISCONNECTED)
+                    .build());
+
+            System.out.println("session is disconnected!!!");
+        }
+    }
+}
